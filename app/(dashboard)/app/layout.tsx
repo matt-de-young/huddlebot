46572f47a5120ca6e37f8a0b74d95e1c5ac2be67
Matt de Young <matt.deyoung@ivy-solutions.de>
2025-03-19 20:46:09 +0100

    Fix previous commit

---
+++ app/(dashboard)/app/layout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export default function SettingsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <div className="flex flex-col min-h-[calc(100dvh-68px)] max-w-7xl mx-auto w-full">
+      <div className="flex flex-1 overflow-hidden h-full">
+        <main className="flex-1 overflow-y-auto p-0 lg:p-4">{children}</main>
+      </div>
+    </div>
+  );
+}
---
+++ app/(dashboard)/app/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from 'next/navigation';
+import { getTeamForUser, getUser } from '@/lib/db/queries';
+
+export default async function SettingsPage() {
+  const user = await getUser();
+
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  const teamData = await getTeamForUser(user.id);
+
+  if (!teamData) {
+    throw new Error('Team not found');
+  }
+
+  return <p>app goes here</p>
+}
