af32a6461f3543881f0c03f9c9fb6df7150b1297
Matt de Young <matt.deyoung@ivy-solutions.de>
2025-03-19 13:24:21 +0100

    Rename to HuddleBot (again)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
 
 export const metadata: Metadata = {
-  title: 'Next.js SaaS Starter',
-  description: 'Get started quickly with Next.js, Postgres, and Stripe.',
+  title: 'HuddleBot',
+  description: 'Automate your daily huddle or standup & stay in deep focus instead.',
 };
 
 export const viewport: Viewport = {
